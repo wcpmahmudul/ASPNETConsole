a8a7af6f6f36e74f742a4971410e2691d8329a87
Mahmudul <mahmudulcsejnu@gmail.com>
2023-05-10 21:39:44 +0600

    Health Check Project Added

---
+++ HealthCheck/Utility/SqlHealthCheck.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Data.SqlClient;
+
+namespace HealthCheck.Utility
+{
+    public class SqlHealthCheck : IHealthCheck
+    {
+        private readonly string _connectionString;
+        public SqlHealthCheck(IConfiguration configuration) 
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using var sqlConnection = new SqlConnection(_connectionString);
+
+                await sqlConnection.OpenAsync(cancellationToken);
+
+                using var command = sqlConnection.CreateCommand();
+                command.CommandText = "SELECT 1";
+
+                await command.ExecuteScalarAsync(cancellationToken);
+
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy(context.Registration.FailureStatus.ToString(), exception: ex);
+            }
+
+        }
+    }
+}
